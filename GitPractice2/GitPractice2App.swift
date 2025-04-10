07758467f9df532709466e205730b72efeb164c8
Manjot Kaur <manjotk2003@gmail.com>
2025-04-10 17:49:22 -0400

    Initial Commit

---
+++ GitPractice2/GitPractice2App.swift
@@ -0,0 +1,17 @@
+//
+//  GitPractice2App.swift
+//  GitPractice2
+//
+//  Created by Manjot Kaur on 4/10/25.
+//
+
+import SwiftUI
+
+@main
+struct GitPractice2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
